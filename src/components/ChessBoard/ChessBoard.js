169d2fa6a65cdfeafd74e3114d9191256d170a52
WebTek828 <webtekprogrammer@gmail.com>
2021-04-13 15:36:55 +0630

    refactored the code

--- src/components/ChessBoard/ChessBoard.js
+++ src/components/ChessBoard/ChessBoard.js
@@ -2,44 +2,11 @@ import React from "react";
 
 import "./ChessBoard.css";
 
+import whiteBoardArr from "./WhiteBoardArr";
+
 import Pieces from "../Pieces/Pieces";
 
 const ChessBoard = (props) => {
-  const white = [
-    1,
-    3,
-    5,
-    7,
-    8,
-    10,
-    12,
-    14,
-    17,
-    19,
-    21,
-    23,
-    24,
-    26,
-    28,
-    30,
-    33,
-    35,
-    37,
-    39,
-    40,
-    42,
-    44,
-    46,
-    49,
-    51,
-    53,
-    55,
-    56,
-    58,
-    60,
-    62,
-  ];
-
   const squareArray = Array.from(Array(64));
 
   const moveThePieceHandler = (index) => {
@@ -52,7 +19,7 @@ const ChessBoard = (props) => {
 
   const square = squareArray.map((square, i) => {
     const squareClsName = `board__square
-   ${white.some((white) => white === i) ? "white" : ""} `.split(" ");
+   ${whiteBoardArr.some((white) => white === i) ? "white" : ""} `.split(" ");
     props.moveAbleSquares.forEach((square) => {
       if (square === i) {
         squareClsName.push("moveable");
